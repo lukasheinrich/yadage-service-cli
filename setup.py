e0bd1f64ddcf2f96475373fdd253b784d84286c9
lukasheinrich <lukas.heinrich@gmail.com>
2017-11-26 19:53:06 +0100

    Bump version: 0.1.8 → 0.1.9

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
   name = 'yadage-service-cli',
-  version = '0.1.8',
+  version = '0.1.9',
   description = 'yadage service command line tools',
   url = 'http://github.com/yadage/yadage-service-cli',
   author = 'Kyle Cranmer, Lukas Heinrich',
